e6bcbdafc304b8c442ec4ddcb81a4f8c3ec51d3d
Nabila <masrurnabila@gmail.com>
2025-01-02 10:49:42 +0600

    files are added

---
+++ index.js
@@ -0,0 +1,10 @@
+var typed =new Typed(".multiple-text",{
+    strings:["Machine Learning","Deep Learning", "Deep Reinforcement Learning", "Computer Vision"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop:true
+})
+
+
+// var typed=new Typed("")
